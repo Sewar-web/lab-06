c425df7519833da663847e6d8f6eb7a8c06c1641
Sewar-web <s.maqableh@yahoo.com>
2021-04-10 23:30:53 +0300

    ssl

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const cors = require('cors');
 const pg = require('pg');
 const server = express();
 const superagent = require('superagent');
-// const { request } = require('express');
 const PORT = process.env.PORT || 2000;
 // const client = new pg.Client(process.env.DATABASE_URL);
 const client = new pg.Client({ connectionString: process.env.DATABASE_URL,ssl:{ rejectUnauthorized: false }});
